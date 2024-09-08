08626a7a91b8493cda5e299cab7b8ba1a09549ce
lucashmunoz <lucashmunoz@gmail.com>
2024-09-08 02:40:04 -0300

    fix: new thunk for recent properties

--- src/store/properties/propertiesSlice.ts
+++ src/store/properties/propertiesSlice.ts
@@ -8,13 +8,21 @@ import { Filters } from "../../context/PropertiesFiltersContext";
 interface PropertyState {
   loadingProperties: boolean
   properties: Property[],
-  isPropertiesError: boolean
+  isPropertiesError: boolean,
+
+  loadingRecentProperties: boolean
+  recentProperties: Property[],
+  isRecentPropertiesError: boolean
 }
 
 const initialState: PropertyState = {
   loadingProperties: true,
   properties: [],
-  isPropertiesError: false
+  isPropertiesError: false,
+
+  loadingRecentProperties: true,
+  recentProperties: [],
+  isRecentPropertiesError: false
 };
 
 interface FetchPropertiesParams {
@@ -87,31 +95,64 @@ export const fetchProperties = createAsyncThunk(
   }
 );
 
+export const fetchRecentProperties = createAsyncThunk(
+  "users/fetchRecentProperties",
+  async (_, { rejectWithValue }) => {
+    const fetchRecentPropertiesUrl = `${API_HOST}${endpoints.properties}?sortBy=RECENT`;
+    try{
+      const response = await api.get(fetchRecentPropertiesUrl);
+      return response.data.content as Property[];
+    }catch(error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
 export const propertiesSlice = createSlice({
   name: "properties",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchProperties.pending, (state) => {
-      state.loadingProperties = true;
-      state.properties = [];
-      state.isPropertiesError = false;
-    });
-    builder.addCase(fetchProperties.fulfilled, (state, action) => {
-      state.loadingProperties = false;
-      state.isPropertiesError = false;
-      state.properties = action.payload;
-    });
-    builder.addCase(fetchProperties.rejected, (state) => {
-      state.loadingProperties = false;
-      state.isPropertiesError = true;
-      state.properties = [];
-    });
+    builder
+      .addCase(fetchProperties.pending, (state) => {
+        state.loadingProperties = true;
+        state.properties = [];
+        state.isPropertiesError = false;
+      })
+      .addCase(fetchProperties.fulfilled, (state, action) => {
+        state.loadingProperties = false;
+        state.isPropertiesError = false;
+        state.properties = action.payload;
+      })
+      .addCase(fetchProperties.rejected, (state) => {
+        state.loadingProperties = false;
+        state.isPropertiesError = true;
+        state.properties = [];
+      })
+      .addCase(fetchRecentProperties.pending, (state) => {
+        state.loadingRecentProperties = true;
+        state.recentProperties = [];
+        state.isRecentPropertiesError = false;
+      })
+      .addCase(fetchRecentProperties.fulfilled, (state, action) => {
+        state.loadingRecentProperties = false;
+        state.isRecentPropertiesError = false;
+        state.recentProperties = action.payload;
+      })
+      .addCase(fetchRecentProperties.rejected, (state) => {
+        state.loadingRecentProperties = false;
+        state.isRecentPropertiesError = true;
+        state.recentProperties = [];
+      });
   }
 });
 
 export const selectProperties = (state: RootState) => state.properties.properties;
 export const selectLoadingProperties = (state: RootState) => state.properties.loadingProperties;
 export const selectIsPropertiesError = (state: RootState) => state.properties.isPropertiesError;
+
+export const selectRecentProperties = (state: RootState) => state.properties.recentProperties;
+export const selectLoadingRecentProperties = (state: RootState) => state.properties.loadingRecentProperties;
+export const selectIsRecentPropertiesError = (state: RootState) => state.properties.isRecentPropertiesError;
 
 export default propertiesSlice.reducer;

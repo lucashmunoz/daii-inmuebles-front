6f573d610596f95c107c3c4ba8fedf4968045786
lucashmunoz <lucashmunoz@gmail.com>
2024-09-08 21:22:19 -0300

    [wip] feat: fetch property details

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import propertiesReducer from "./properties/propertiesSlice";
 import rentalsReducer from "./properties/rentalsSlice";
 import districtsReducer from "./properties/districtsSlice";
+import propertyDetailsReducer from "./properties/propertyDetailsSlice";
 
 export const store = configureStore({
   reducer: {
     properties: propertiesReducer,
+    propertyDetails: propertyDetailsReducer,
     rentals: rentalsReducer,
     districts: districtsReducer
   }

94f7598234fc91acd564e1c14c68fdba4191b533
lucashmunoz <lucashmunoz@gmail.com>
2024-09-15 16:25:33 -0300

    feat: toggle my property activate switch

---
+++ src/ui/pages/EditProperty/EditProperty.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import PageWrapper from "../../components/PageWrapper";
+
+const EditProperty = () => {
+  return (
+    <PageWrapper>
+      <Header />
+      <h1>Editar propiedad</h1>
+    </PageWrapper>
+  );
+};
+
+export default EditProperty;

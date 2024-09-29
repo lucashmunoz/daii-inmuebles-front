6a82ea8ea403b0ff034de3b143ad2578e6a67495
Alejandro Susmelj <110204705+alesusmelj@users.noreply.github.com>
2024-09-29 19:27:32 -0300

    feat: add price estimation on property details (#63)

--- src/store/properties/propertyDetailsSlice.ts
+++ src/store/properties/propertyDetailsSlice.ts
@@ -4,9 +4,16 @@ import { endpoints } from "../../api/endpoints";
 import api, { API_HOST } from "../../api/api";
 import { Property } from "../../models/property";
 
+interface PricePrediction {
+  classification: string;
+  estimated_price: number;
+}
+
 interface PropertyDetailsState {
-  propertyDetails: Property,
-  propertyDetailsStatus: "SUCCESS" | "ERROR" | "LOADING"
+  propertyDetails: Property;
+  propertyDetailsStatus: "SUCCESS" | "ERROR" | "LOADING";
+  pricePrediction: PricePrediction;
+  pricePredictionStatus: "NOT_INITIALIZED" | "SUCCESS" | "ERROR" | "LOADING";
 }
 
 const initialState: PropertyDetailsState = {
@@ -26,9 +33,7 @@ const initialState: PropertyDetailsState = {
     district: "",
     latitude: 0,
     longitude: 0,
-    images: [
-      ""
-    ],
+    images: [""],
     address: "",
     storeys: 0,
     price: 0,
@@ -37,21 +42,39 @@ const initialState: PropertyDetailsState = {
     surface_covered: 0,
     surface_total: 0
   },
-  propertyDetailsStatus: "LOADING"
+  propertyDetailsStatus: "LOADING",
+  pricePrediction: {
+    classification: "",
+    estimated_price: 0
+  },
+  pricePredictionStatus: "NOT_INITIALIZED"
 };
 
 interface FetchPropertyDetailsPayload {
-  propertyId: number
+  propertyId: number;
 }
 
 export const fetchPropertyDetails = createAsyncThunk(
   "users/fetchPropertyDetails",
   async ({ propertyId }: FetchPropertyDetailsPayload, { rejectWithValue }) => {
     const fetchPropertyDetailsUrl = `${API_HOST}${endpoints.properties}/${propertyId}`;
-    try{
+    try {
       const response = await api.get(fetchPropertyDetailsUrl);
       return response.data as Property;
-    }catch(error) {
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchPropertyPricePrediction = createAsyncThunk(
+  "users/fetchPropertyPricePrediction",
+  async (propertyId: number, { rejectWithValue }) => {
+    const fetchPricePredictionUrl = `${API_HOST}${endpoints.predictPrice}/${propertyId}`;
+    try {
+      const response = await api.get(fetchPricePredictionUrl);
+      return response.data as PricePrediction;
+    } catch (error) {
       return rejectWithValue(error);
     }
   }
@@ -72,7 +95,31 @@ export const propertyDetailsSlice = createSlice({
     builder
       .addCase(fetchPropertyDetails.pending, (state) => {
         state.propertyDetailsStatus = "LOADING";
-        state.propertyDetails = {} as Property;
+        state.propertyDetails = {
+          id: 0,
+          beds: 0,
+          bathrooms: 0,
+          country: "",
+          active: true,
+          created_at: "",
+          favorite: false,
+          city: "",
+          state: "",
+          rooms: 0,
+          title: "",
+          description: "",
+          district: "",
+          latitude: 0,
+          longitude: 0,
+          images: [""],
+          address: "",
+          storeys: 0,
+          price: 0,
+          garages: 0,
+          type: "APARTMENT",
+          surface_covered: 0,
+          surface_total: 0
+        };
       })
       .addCase(fetchPropertyDetails.fulfilled, (state, action) => {
         state.propertyDetailsStatus = "SUCCESS";
@@ -80,13 +127,62 @@ export const propertyDetailsSlice = createSlice({
       })
       .addCase(fetchPropertyDetails.rejected, (state) => {
         state.propertyDetailsStatus = "ERROR";
-        state.propertyDetails = {} as Property;
+        state.propertyDetails = {
+          id: 0,
+          beds: 0,
+          bathrooms: 0,
+          country: "",
+          active: true,
+          created_at: "",
+          favorite: false,
+          city: "",
+          state: "",
+          rooms: 0,
+          title: "",
+          description: "",
+          district: "",
+          latitude: 0,
+          longitude: 0,
+          images: [""],
+          address: "",
+          storeys: 0,
+          price: 0,
+          garages: 0,
+          type: "APARTMENT",
+          surface_covered: 0,
+          surface_total: 0
+        };
+      })
+      .addCase(fetchPropertyPricePrediction.pending, (state) => {
+        state.pricePredictionStatus = "LOADING";
+        state.pricePrediction = {
+          classification: "",
+          estimated_price: 0
+        };
+      })
+      .addCase(fetchPropertyPricePrediction.fulfilled, (state, action) => {
+        state.pricePredictionStatus = "SUCCESS";
+        state.pricePrediction = action.payload;
+      })
+      .addCase(fetchPropertyPricePrediction.rejected, (state) => {
+        state.pricePredictionStatus = "ERROR";
+        state.pricePrediction = {
+          classification: "",
+          estimated_price: 0
+        };
       });
   }
 });
 
-export const selectPropertyDetails = (state: RootState) => state.propertyDetails.propertyDetails || {};
-export const selectPropertyDetailsStatus = (state: RootState) => state.propertyDetails.propertyDetailsStatus;
+export const selectPropertyDetails = (state: RootState) =>
+  state.propertyDetails.propertyDetails || {};
+export const selectPropertyDetailsStatus = (state: RootState) =>
+  state.propertyDetails.propertyDetailsStatus;
+export const selectPricePrediction = (state: RootState) =>
+  state.propertyDetails.pricePrediction;
+export const selectPricePredictionStatus = (state: RootState) =>
+  state.propertyDetails.pricePredictionStatus;
 
-export const { bookmarkProperty, unbookmarkProperty } = propertyDetailsSlice.actions;
+export const { bookmarkProperty, unbookmarkProperty } =
+  propertyDetailsSlice.actions;
 export default propertyDetailsSlice.reducer;

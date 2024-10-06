a133a54a5afcb3e8456d3025d3ea055a8a7d728f
JulianOubina <oubinajulian@gmail.com>
2024-10-06 16:33:35 -0300

    feat: delete keys on deploy workflow

--- main.tf
+++ main.tf
@@ -8,37 +8,19 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-  token      = var.token
-}
-
-# Variables para las credenciales de AWS
-variable "access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
-variable "token" {
-  description = "AWS Session Token (optional)"
-  type        = string
-  default     = ""
-}
-
-variable "key_name" {
-  description = "El nombre de la clave"
-  default     = "smartmove"
+  region                   = "us-east-1"
+  shared_credentials_file  = "~/.aws/credentials"
+  profile                 = "default"  # Cambia esto si usas un perfil diferente
 }
 
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "El nombre de la clave"
+  default     = "smartmove"
 }
 
 resource "aws_key_pair" "key_pair" {
@@ -52,7 +34,7 @@ resource "local_file" "private_key" {
 }
 
 resource "aws_instance" "Frontend" {
-  ami           = "ami-0866a3c8686eaeeba"  # Verifica que esta AMI esté disponible en us-east-1
+  ami           = "ami-0866a3c8686eaeeba"  # Asegúrate de que esta AMI esté disponible en us-east-1
   instance_type = "t2.micro"
   key_name      = aws_key_pair.key_pair.key_name
 

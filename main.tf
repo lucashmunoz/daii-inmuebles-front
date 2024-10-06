7d44f4b37a4e0aeeee50edb4a2b6dad26b91f2a1
JulianOubina <oubinajulian@gmail.com>
2024-10-06 15:27:51 -0300

    feat: add main terraform

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token      = var.token
+}
+
+# Variables para las credenciales de AWS
+variable "access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
+variable "token" {
+  description = "AWS Session Token (optional)"
+  type        = string
+  default     = "" 
+}
+
+variable "key_name" {
+  description = "El nombre de la clave"
+  default     = "smartmove"
+}
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = "${var.key_name}.pem"
+}
+
+resource "aws_instance" "Frontend" {
+  ami           = "ami-0866a3c8686eaeeba" 
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "Frontend"
+  }
+}
+

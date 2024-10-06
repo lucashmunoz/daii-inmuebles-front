876f5f439a6110f2b81a2cd2b4717367b29096c3
JulianOubina <oubinajulian@gmail.com>
2024-10-06 15:50:29 -0300

    fix: variables on main.tf

--- main.tf
+++ main.tf
@@ -14,45 +14,33 @@ provider "aws" {
   token      = var.AWS_SESSION_TOKEN
 }
 
-# Variables para las credenciales de AWS
-variable "access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
-variable "token" {
-  description = "AWS Session Token (optional)"
-  type        = string
-  default     = "" 
-}
-
+# Variable para el nombre de la clave
 variable "key_name" {
   description = "El nombre de la clave"
   default     = "smartmove"
 }
 
+# Generación de clave privada con TLS
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Crear un par de claves (key pair) en AWS
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
+# Guardar la clave privada localmente
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = "${var.key_name}.pem"
 }
 
+# Crear una instancia EC2
 resource "aws_instance" "Frontend" {
-  ami           = "ami-0866a3c8686eaeeba" 
+  ami           = "ami-0866a3c8686eaeeba"  # Verifica que esta AMI esté disponible en us-east-1
   instance_type = "t2.micro"
   key_name      = aws_key_pair.key_pair.key_name
 
@@ -60,4 +48,3 @@ resource "aws_instance" "Frontend" {
     Name = "Frontend"
   }
 }
-
